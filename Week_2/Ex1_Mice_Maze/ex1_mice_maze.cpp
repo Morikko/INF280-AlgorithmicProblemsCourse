930d2e8256823f02d3faff76569e3adfe5a18b79
Eric Masseran <rico.masseran@gmail.com>
2015-05-17 19:58:18 +0200

    Example is good but submission is wrong

--- Week_2/Ex1_Mice_Maze/ex1_mice_maze.cpp
+++ Week_2/Ex1_Mice_Maze/ex1_mice_maze.cpp
@@ -6,15 +6,14 @@
 using namespace std;
 
 bool debug = false;
-int cells, finish, counter;
-int nbr_mice = 0;
-int connections[100][100];
-// True if the mouse in the cell can reach the exit
-bool reach[100];
-// How many times should take a nouse to reach the exit
-int weight[100];
-
-queue<int> nextCell;
+        int cells, finish, counter;
+        int nbr_mice = 0;
+        int connections[100][100];
+        // True if the mouse in the cell can reach the exit
+        bool reach[100];
+        // How many times should take a nouse to reach the exit
+        int weight[100];
+        queue<int> nextCell;
 
 int main(int argc, char** argv){
     int cases;
@@ -37,20 +36,20 @@ int main(int argc, char** argv){
             cin >> a;
             cin >> b;
             cin >> val;
-            connections[a][b] = val;
+            connections[a-1][b-1] = val;
             if(debug)
                 cout << "From " << a << " to " << b << " is " << val << endl;
         }
 
         // Start from the end
-        nextCell.push(finish);
+        nextCell.push(finish-1);
         // Add mouse on case finish
         nbr_mice++;
         // While a mouse can reach the end from a cell
         while(!nextCell.empty()){
             int current = nextCell.front();
             nextCell.pop();
-            for(int a=0; a<100; a++){
+            for(int a=0; a<cells; a++){
                 if(connections[a][current] != 0){
                     // In the time
                     if(connections[a][current]+weight[current] <= counter){
@@ -60,14 +59,23 @@ int main(int argc, char** argv){
                             reach[a] = true;
                         }
                         // Update weight
-                        if(weight[a] == 0 || connections[a][current]+weight[current]<weight[a])
+                        if(weight[a] == 0 || connections[a][current]+weight[current]<weight[a]){
                             weight[a] = connections[a][current]+weight[current];
+                            nextCell.push(a);
+                        }
                    }
                 }
             }
         }
 
         cout << nbr_mice << endl;
+        // Reset array
+        for(int i=0; i<cells; i++){
+            for(int j=0; j<cells; j++)
+                connections[i][j] = 0;
+            reach[i] = false;
+            weight[i] = 0;
+        }
     }    
     return 0;
 }
