281ee33b57e45acfecb8036e04d2545c4c5d0b0e
Eric Masseran <rico.masseran@gmail.com>
2015-05-17 10:25:00 +0200

    Add exercices for week 2

---
+++ Week_2/Ex2_Project_File_Dependencies/ex2_makefile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+bool debug = true;
+
+int main(int argc, char** argv){
+    while(true){
+        /* Read the entry */
+    }    
+    return 0;
+}
