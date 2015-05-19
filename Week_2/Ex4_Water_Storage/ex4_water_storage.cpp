eff5aab5da503d7074ed8f734177c8936a173f4d
Eric Masseran <rico.masseran@gmail.com>
2015-05-19 03:07:19 +0200

    Exercice 4 works and is accepted

--- Week_2/Ex4_Water_Storage/ex4_water_storage.cpp
+++ Week_2/Ex4_Water_Storage/ex4_water_storage.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
+#include <cfloat>
 
 using namespace std;
 
-bool debug = true;
+bool debug = false;
 float level[100], height[100], width[100], depth[100], volume;
 
 void heapsort(float* tab, float* tab1, float* tab2, float* tab3, int max);
@@ -14,7 +16,7 @@ void inverse(float* val1, float* val2);
 int main(int argc, char** argv){
     int cases;
     cin >> cases;
-
+    cout << std::fixed << std::setprecision(2);
     for(int i=0; i<cases; i++){
         /* Read the entry */
         int nbr_cisterns;
@@ -37,12 +39,66 @@ int main(int argc, char** argv){
         }   
 
         float total_level = 0;
+        int cistern = 0;
+        float min_level, min_height, next_level, current_height, eq_vol;
+        while(volume>0 && cistern < nbr_cisterns){
+            while(level[cistern] == 0 && height[cistern] == 0)
+                cistern++;
+            min_level = level[cistern];
+            // Only support
+            if(min_level>0){
+                for(int c=cistern; c<nbr_cisterns; c++){
+                    level[c] -= min_level;
+                }
+                total_level += min_level;
+            // We touch a cistern
+            }else{
+                // Take the smallest cistern and the next smallest height
+                min_height = FLT_MAX;
+                for(int c=cistern; c<nbr_cisterns-1 && level[c]==0; c++){
+                    if(height[c] < min_height)
+                        min_height = height[c];
+                    if(level[c+1]>0){
+                        next_level = level[c+1];
+                        break;
+                    }
+                }
+                
+                // Take the smallest height before change
+                // Cistern is smaller than height
+                if(min_height<next_level){
+                    current_height = min_height;
+                // Height is bigger then cistern
+                }else{
+                    current_height = next_level;
+                }
+                
+                // Find equivalent volume for all cisterns at the level
+                eq_vol = 0;
+                for(int c=cistern; c<nbr_cisterns && level[c]==0; c++){
+                    if(height[c] > 0){
+                        eq_vol += current_height * width[c] * depth[c];
+                    } 
+                }
 
-        while(volume>0){
+                // Update value
+                // End
+                if(eq_vol > volume){
+                    total_level += (volume/eq_vol) * current_height;
+                    volume = 0;
+                }else{
+                    total_level += current_height;
+                    volume -= eq_vol;
+                }
+               
+                for(int c=cistern; c<nbr_cisterns; c++){
+                    if(level[c]>0)
+                        level[c] -= current_height;
+                    else
+                        height[c] -= current_height;
+                }
 
-            
-            break;
-
+            }
         }
 
         if(volume==0)
