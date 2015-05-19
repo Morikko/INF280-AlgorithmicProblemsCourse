599206ec9c9463d64e613ae6a4d1c187c99ae3fc
Eric Masseran <rico.masseran@gmail.com>
2015-05-20 00:29:33 +0200

    Order everything in Exercice 2 but still not accepted

--- Week_2/Ex2_Project_File_Dependencies/ex2_makefile.cpp
+++ Week_2/Ex2_Project_File_Dependencies/ex2_makefile.cpp
@@ -9,9 +9,16 @@ bool debug = true;
 int tasks, rules;
 
 vector<int> task_order;
+int target_task[100];
+int dependencies[100];
+int depend[100][100];
 
 vector<int> insertBefore(int ref, int target);
 bool isAfter(int ref, int target);
+
+void heapsort(int* tab, int* tab1, int* tab2, int max);
+void tam(int *tab, int* tab1, int* tab2, int node, int n);
+void inverse(int* val1, int* val2);
 
 int main(int argc, char** argv){
     int cases;
@@ -24,25 +31,38 @@ int main(int argc, char** argv){
         // Initiate tasks order in ...
         for(int t=0; t<tasks; t++)
             task_order.push_back(t+1);
+       
+        // Get dependencies 
+        for(int r=0; r<rules; r++){
+            cin >> target_task[r];
+            cin >> dependencies[r];
+
+            for(int d=0; d<dependencies[r]; d++){
+                cin >> depend[r][d];
+            }
+        }
+        int temp[100];
+        for(int k=0; k<100; k++)
+            temp[k] = k;
+
+        // Order
+        heapsort(target_task, dependencies, temp, rules);
         
         // Threat the rukes
         for(int r=0; r<rules; r++){
-            int target_task;
-            cin >> target_task;
-            int dependencies;
-            cin >> dependencies;
-
-            for(int d=0; d<dependencies; d++){
-                int depen;
-                cin >> depen;
-                if(isAfter(target_task, depen))
-                    task_order = insertBefore(target_task, depen);
-
+            heapsort(depend[temp[r]], NULL, NULL, dependencies[r]);
+            for(int d=0; d<dependencies[r]; d++){
+                if(isAfter(target_task[r], depend[temp[r]][d]))
+                    task_order = insertBefore(target_task[r], depend[temp[r]][d]);
             }
-
         }
-        for(int t=0; t<tasks; t++)
-           cout << task_order[t] << " ";
+        
+        // Print output
+        for(int t=0; t<tasks; t++){
+            cout << task_order[t];
+            if(t<tasks-1)
+                cout << " ";
+        }
 
        cout << endl; 
        if(c<cases-1){
@@ -79,3 +99,39 @@ vector<int> insertBefore(int ref, int target){
    return temp;
 }
 
+void heapsort(int* tab, int* tab1, int* tab2, int max){
+    for(int i((max-1)/2); i>=0; i--)
+        tam(tab, tab1, tab2, i, max-1);
+    for(int i(max-1); i>=1; i--){
+        inverse(&tab[i], &tab[0]);
+        if(tab1 != NULL)
+            inverse(&tab1[i], &tab1[0]);
+        if(tab2 != NULL)
+            inverse(&tab2[i], &tab2[0]);
+        tam(tab, tab1, tab2, 0, i-1);
+    }
+}
+
+void tam(int *tab, int* tab1, int* tab2, int node, int n){
+    int k = node, j = 2*k;
+    while(j<=n){
+        if(j < n && tab[j] < tab[j+1])
+            j++;
+        if(tab[k] < tab[j]){
+            inverse(&tab[k], &tab[j]);
+            if(tab1 != NULL)
+                inverse(&tab1[k], &tab1[j]);
+            if(tab2 != NULL)
+                inverse(&tab2[k], &tab2[j]);
+            k = j;
+            j = 2*k;
+        }else
+            break;
+   }
+}
+
+void inverse(int* val1, int* val2){
+    int temp = *val1;
+    *val1 = *val2;
+    *val2 = temp;
+}
