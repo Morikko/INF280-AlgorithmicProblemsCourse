9cea729ebfa4cbe3538e37a2dde4f086485cbc81
Eric Masseran <rico.masseran@gmail.com>
2015-05-25 19:58:49 +0200

    Exercice 5 works but raise a Runtime error

--- Week_3/Ex5_Men_at_Work/ex5_men_at_work.cpp
+++ Week_3/Ex5_Men_at_Work/ex5_men_at_work.cpp
@@ -2,11 +2,31 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-bool debug = true;
+bool debug = false;
 int calc_ppcm(int X, int Y);
+struct coord{
+    int x;
+    int y;
+};    
+struct d_info{
+    coord c;
+    int dimension;
+    int length;
+};
+
+class CompareD {
+    public:
+        bool operator()(d_info& d1, d_info& d2){
+            if(d1.length < d2.length)
+                return false;
+            else
+                return true;
+        }
+};
 
 int main(int argc, char** argv){
     string temp;
@@ -76,7 +96,7 @@ int main(int argc, char** argv){
             cout << "PPCM : " << ppcm << endl;
 
         // Create 3-dimension graph
-        vector< vector< vector<bool> > > available(ppcm, vector< vector<bool> >(size, vector<bool>(size, false)));
+        vector< vector< vector<bool> > > available(ppcm, vector< vector<bool> >(size, vector<bool>(size, true)));
         vector< vector<int> > cpt(size, vector<int>(size, 0));
         for(int i=1; i<ppcm; i++){
             graph.push_back(vector< vector<bool> >(size, vector<bool>(size)));
@@ -95,8 +115,61 @@ int main(int argc, char** argv){
             //cout << endl << endl;
        }
 
-
         // Search shortest path
+        priority_queue<d_info, vector<d_info>, CompareD> paths;
+        paths.push((d_info){{0, 0}, 0, 0});
+        bool not_finished = true;
+        available[0][0][0] = false;
+
+        while(!paths.empty() && not_finished){
+            int x = paths.top().c.x;
+            int y = paths.top().c.y;
+            int dimension = (paths.top().dimension+1)%ppcm;
+            int length = paths.top().length;
+            // Search next available node
+            vector<coord> next_cell;
+            // Add down
+            if(y < size-1){
+                if(graph[dimension][y+1][x] && available[dimension][y+1][x])
+                    next_cell.push_back((coord){x, y+1});
+            }
+            // Add left
+            if(x < size -1){
+                if(graph[dimension][y][x+1] && available[dimension][y][x+1])
+                    next_cell.push_back((coord){x+1, y});
+            }
+            // Add wait
+            if(graph[dimension][y][x] && available[dimension][y][x])
+                next_cell.push_back((coord){x, y});
+            // Add right
+            if(x > 0){
+                if(graph[dimension][y][x-1] && available[dimension][y][x-1])
+                    next_cell.push_back((coord){x-1, y});
+            }
+            // Add up
+            if(y > 0){
+                if(graph[dimension][y-1][x] && available[dimension][y-1][x])
+                    next_cell.push_back((coord){x, y-1});
+            }
+            for(int i=0; i<next_cell.size();i++){
+                // Good solution
+                if(next_cell[i].y == size-1 && next_cell[i].x == size-1){
+                    cout << (length+1) << endl;
+                    not_finished = false;
+                    break;
+                }
+                // Continue looking
+                else{
+                    paths.push((d_info){{next_cell[i].x, next_cell[i].y}, dimension, length+1});
+                    available[dimension][next_cell[i].y][next_cell[i].x] = false;
+                }
+            }
+            // Remove
+            paths.pop();
+        }
+        // No solution
+        if(paths.empty() && not_finished)
+            cout << "NO" << endl;
        
         if(debug)
             cout << "---------------------------" << endl;
